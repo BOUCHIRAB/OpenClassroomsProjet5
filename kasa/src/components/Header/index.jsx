1da93404ff2264e276960ba4fc462fd4a9c4ec55
BOUCHIRAB <ib.bouchirab@yahoo.fr>
2024-04-26 16:43:41 +0200

    Vérification SCSS

--- kasa/src/components/Header/index.jsx
+++ kasa/src/components/Header/index.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "react-router-dom"
-import logo from "../../assets/LOGO.webp"
+import Logo from "../../assets/LOGO.webp"
 import "./Header.scss"
 import styled from "styled-components"
 
@@ -16,7 +16,7 @@ function Header() {
   return (
     <header>
       <nav className="navbar">
-        <img src={logo} alt="logo kasa" className="navbar_img"></img>
+        <img src={Logo} alt="logo kasa" className="navbar_img"></img>
         <div className="navbar_link ">
           <StyledLink to="/">Accueil</StyledLink>
           <StyledLink to="/Apropos/">A Propos</StyledLink>
